162bb1c7e31306b20d168eea26742f37bf1d8c09
Matthew Loop <matthew_loop@unc.edu>
2019-11-27 11:32:52 -0500

    Summarize abstracts by common activity

--- scripts/01_summarize_abstracts.R
+++ scripts/01_summarize_abstracts.R
@@ -19,3 +19,25 @@ abstracts %>%
   geom_col() +
   xlab(NULL) +
   coord_flip()
+
+abstracts_activity <- nih %>%
+  select(`Project Title`, `Project Abstract`, Activity) %>%
+  unnest_tokens(word, `Project Abstract`) %>%
+  anti_join(stop_words) %>%
+  filter(!(word %in% c("project", "summary", "abstract", "rationale", "1", "2", "3", "aims", "research", "studies", "specific", "study", "data", "proposal", "proposed", "goal", "aim")))
+
+common_activities <- nih %>%
+  count(Activity, sort = TRUE) %>%
+  slice(1:20)
+
+abstracts_activity %>%
+  group_by(Activity) %>%
+  count(word, sort = TRUE) %>%
+  slice(1:10) %>%
+  left_join(select(common_activities, -n), ., by = "Activity") %>%
+  mutate(word = factor(word) %>% fct_reorder(n)) %>%
+  ggplot(aes(word, n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip() +
+  facet_wrap(~ Activity, scales = "free")
